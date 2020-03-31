607b7638f23c1e12f7cf0e8705fe5e8371f37202
IRTAZA <irtaza.syed@daimler.com>
2020-03-31 14:22:10 +0200

    Sweetpopup

--- main.js
+++ main.js
@@ -1,79 +1,91 @@
-$(function(){ 
-    $(document).ready(function() { $('body').bootstrapMaterialDesign(); });
-    var date1;
-    var date2;
-    var rolldate1 = new Rolldate({
-        el: '#example',
-        lang: { 
-          title: 'Select A Date', 
-          cancel: 'Cancel', 
-          confirm: 'Confirm', 
-          year: '', 
-          month: '', 
-          day: '', 
-          hour: '', 
-          min: '', 
-          sec: '' 
-        },
-        confirm: function(date) {
-            date1=date;
-            console.log(date);
-          },
-    });
+$(function () {
+  $(document).ready(function () { $('body').bootstrapMaterialDesign(); });
+  var dateInit;
+  var dateCurrent;
+  new Rolldate({
+    el: '#dateInit',
+    lang: {
+      title: 'Select A Date',
+      cancel: 'Cancel',
+      confirm: 'Confirm',
+      year: '',
+      month: '',
+      day: '',
+      hour: '',
+      min: '',
+      sec: ''
+    },
+    confirm: function (date) {
+      dateInit = date;
+      console.log(date);
+    },
+  });
 
-    var rolldate2 = new Rolldate({
-        el: '#example1',
-        lang: { 
-          title: 'Select A Date', 
-          cancel: 'Cancel', 
-          confirm: 'Confirm', 
-          year: '', 
-          month: '', 
-          day: '', 
-          hour: '', 
-          min: '', 
-          sec: '' 
-        },
-        confirm: function(date) {
-            date2=date;
-            console.log(date);
-          },
-    });
-
-    $("#basicDate").flatpickr({
-        enableTime: false,
-        
-        dateFormat: "F, d Y"
-    });
-    $("#basicDate2").flatpickr({
-        enableTime: false,
-        
-        dateFormat: "F, d Y"
-    });
-    $('.collapse').collapse()
-    var calendar = flatpickr("#mycalendar");
-    // ...        
-    var fp = flatpickr("#basicDate", {}); // flatpickr
-        var fp1 = flatpickr("#basicDate2", {}); // flatpickr
+  new Rolldate({
+    el: '#dateCurrent',
+    lang: {
+      title: 'Select A Date',
+      cancel: 'Cancel',
+      confirm: 'Confirm',
+      year: '',
+      month: '',
+      day: '',
+      hour: '',
+      min: '',
+      sec: ''
+    },
+    confirm: function (date) {
+      dateCurrent = date;
+      console.log(date);
+    },
+  });
 
 
-    $('#buttoncalc').on('click', function(event) {
-
-    const date12 = new Date(date1);
-    const date23 = new Date(date2);
-    const diffTime = Math.abs(date23 - date12);
+  $('#buttoncalc').on('click', function (event) {
+    if( !$('#volumeInit').val() || !$('#volumeCurrent').val() || !$('#dateInit').val() || !$('#dateCurrent').val()) {
+      Swal.fire({
+        icon: 'error',
+        text: 'Please fill all fields!',
+      })
+}
+else{
+    const dateInitFinal = new Date(dateInit);
+    const dateCurrentFinal = new Date(dateCurrent);
+    const diffTime = Math.abs(dateCurrentFinal - dateInitFinal);
     console.log(diffTime);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-    const input1 = $("#input1").val()
-    const input2 = $("#input2").val()
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const input1 = $("#volumeInit").val()
+    const input2 = $("#volumeCurrent").val()
     const volume = input2 - input1;
-    const result = volume/diffDays;
+    const result = volume / diffDays;
 
-    const result1 = (volume/input1)/diffDays;
-    $("#abs_result").html("Absolute Growth: "+result+"ml")
-    $("#rel_result").html("Relative Growth: "+result1+"%")
-        
-      });
+    const result1 = (volume / input1) / diffDays;
+    Swal.fire({
+      html:
+        '<b>Absolute growth: </b> ' + result + 'ml' + '<br>' +
+        '<b>Relative growth: </b> ' + result1 + '%' + '<br>',
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      cancelButtonText: 'Reset'
+    }).then((result) => {
+      if (!result.value) {
+resetInput();
+      }
+    })
+  }
+  });
+
+  $('#buttonreset').on('click', function (event) {
+resetInput();
+  });
+  function resetInput(){
+    $('#dateInit').val('');
+    $('#dateCurrent').val('');
+    $('#volumeInit').val('');
+    $('#volumeCurrent').val('');
+  }
+
 
 })
 
